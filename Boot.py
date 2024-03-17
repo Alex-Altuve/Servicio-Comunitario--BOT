3e883399aca39fc49387f5a601a96f493182120b
Alex-Altuve <altuvealex6@gmail.com>
2024-03-17 18:06:07 -0400

    Se modifico el codigo para adaptarlo al navegador Chrome, se añadio una ventana para que el usuaio decida la cantidad de PDF a imprimir en la ventana con los formularios, ademas de que ya se tiene lo del nombre listo en el formato pedido

--- Boot.py
+++ Boot.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.options import Options
+import keyboard
+import pyautogui  as pa
+import random
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime, timedelta
 
 def alerta(mensaje_notificacion):
     # Ejecutar JavaScript para mostrar una notificación
@@ -9,19 +15,71 @@ def alerta(mensaje_notificacion):
     driver.execute_script(script)
     time.sleep(10)
 
+def nombrepdf():
+    copiando_nombre=driver.find_element(By.CSS_SELECTOR, '#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div.submission-data-table > div.submission-data-table__row.submission-data-table__row--group.submission-data-table__row--type-group_root > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(2) > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(16) > div.submission-data-table__column.submission-data-table__column--data > div')
+    texto=copiando_nombre.text
+    copiando_fecha=driver.find_element(By.CSS_SELECTOR, '#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div.submission-data-table > div.submission-data-table__row.submission-data-table__row--group.submission-data-table__row--type-group_root > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(2) > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(8) > div.submission-data-table__column.submission-data-table__column--data > div')
+    texto2=copiando_fecha.text
+    texto2=modificar_fecha(texto2)
+    print(texto+texto2)
+
+def modificar_fecha(fecha):
+    # Definir mapeo de nombres de meses en español a números de mes
+    meses = {
+        'ene': '01',
+        'feb': '02',
+        'mar': '03',
+        'abr': '04',
+        'may': '05',
+        'jun': '06',
+        'jul': '07',
+        'ago': '08',
+        'sep': '09',
+        'oct': '10',
+        'nov': '11',
+        'dic': '12'
+    }
+
+    # Convertir el string de entrada a minúsculas
+    fecha = fecha.lower()
+
+    # Obtener día, mes y año de la fecha
+    partes = fecha.split()
+    dia = partes[0]
+    mes = partes[2].rstrip('.')  # Eliminar el punto al final del nombre del mes
+    anio = partes[4]
+
+    # Convertir la fecha al formato AAAAMMDD
+    fecha_modificada = anio + meses[mes] + dia
+
+    # Restar un día a la fecha
+    fecha_dt = datetime.strptime(fecha_modificada, '%Y%m%d')
+    fecha_modificada = (fecha_dt + timedelta(days=1)).strftime('%Y%m%d')
+
+    return fecha_modificada
+
+def pasar_siguiente():
+
+    siguiente=driver.find_element(By.CLASS_NAME, 'submission-pager')
+    siguiente.click()
+    time.sleep(6)
+    pass
+
+
 #------VENTANA 0
 # Ruta al controlador de Microsoft Edge (descárgalo previamente desde https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
 # No funciono asi que lo deje asi pero hay que verificar como hacer esto 
-
+print(pa.size())#en la mia es 1920x1080
+print(pa.position())#(x=143, y=961) esto para darle al boton imprimir, (x=170, y=435)Para escribir el nombre, (x=793, y=504)para guardarlo con el nombre 
 # Inicializa las opciones del navegador Edge
+# Inicializa las opciones del navegador Chrome
 options = Options()
-options.use_chromium = True
 options.add_argument("--disable-features=EnableEphemeralFlashPermission")
 
 
-# Crea el controlador del navegador Edge con las opciones configuradas
-# Inicializa el navegador Microsoft Edge
-driver = webdriver.Edge(options=options)
+# Crea el controlador del navegador Chrome con las opciones configuradas
+# Inicializa el navegador Microsoft chrome
+driver = webdriver.Chrome(options=options)
 #Esto es para inicializarlo sin configuraciones 
 #driver = webdriver.Edge()
 
@@ -30,14 +88,12 @@ driver.get('https://eu.kobotoolbox.org/accounts/login/')
 #Se maximiza la pantalla
 driver.maximize_window()
 
-
-alerta("Debe ingresar su usuario y contraseña para que el bot pueda continuar")
-# Pausa para que el usuario ingrese su usuario y contraseña
-while True:
-    if driver.current_url != 'https://eu.kobotoolbox.org/accounts/login/':
-        break
-    time.sleep(0.1)
-
+time.sleep(6)
+usuario=driver.find_element(By.NAME, "login")
+usuario.send_keys('pasante_monitoreo')
+contrasena=driver.find_element(By.NAME,'password')
+contrasena.send_keys('1/fyV2g(H1h')
+contrasena.submit()
 # Esperar a que la página se cargue completamente
 driver.implicitly_wait(10)
 
@@ -69,62 +125,50 @@ if validacion == True:
     time.sleep(6) 
     enlace_datos_form= driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper > div.mdl-layout__content.page-wrapper__content.page-wrapper__content--form-landing > nav > ul > li:nth-child(3)")
     enlace_datos_form.click()
-    
-    # Esperar a que la página se cargue completamente para agarrar los check_box
-    # driver.implicitly_wait(10)
-    # checkboxesFiltrados = []
-    # checkboxes = driver.find_elements(By.CLASS_NAME, "checkbox__input")
-    
-    # for i in range(1, len(checkboxes)):
-    #   checkboxes[2].click()
-    
-    #Aqui se pondra el mensaje para la eleccion de la cantidad de form a imprimir 
-    # alerta("ESPERA PA PROBAR")
-    # while True:
-    #     if driver.current_url != 'https://eu.kobotoolbox.org/#/forms/a8b3ib9YmME5azzk6c5ijh/data/table':
-    #         break
-    # time.sleep(0.1)
-
-    time.sleep(10) 
-    check_principal = driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper > div.mdl-layout__content.page-wrapper__content.page-wrapper__content--form-landing > div.form-view.form-view--table > div.ReactTable.-highlight > div.rt-table > div.rt-thead.-filters > div > div.rt-th.rt-sub-actions.is-frozen.is-last-frozen > div > div.checkbox > label > input")
-    check_principal.click()
-
+    #Se espera a que cargue
     time.sleep(6) 
-    boton_ojo = driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper > div.mdl-layout__content.page-wrapper__content.page-wrapper__content--form-landing > div.form-view.form-view--table > div.ReactTable.-highlight > div.rt-table > div.rt-tbody > div:nth-child(1) > div > div.rt-td.rt-sub-actions.is-frozen.is-last-frozen > div > button:nth-child(2)")
-    boton_ojo.click()
+    ojitos = driver.find_elements(By.CSS_SELECTOR,'button[data-tip="Abrir"]')
+  
+    numero_ingresado = int(pa.prompt(text='Por favor, ingrese un número desde donde iniciar', default='0'))
+    print("Número ingresado:", numero_ingresado)
 
-    time.sleep(8) 
+#    # #numero_pdf=int(input("Por favor, ingrese la cantidad de archivos a descargar: "))
+    #seleccionamos el primer ojito para llegar al que queremos esto por ahora
+    ojitos[0].click() 
+    time.sleep(6)
+    #le damos tantas veces a siguiente para llegar al archivo que queramos 
+    for i in  range(1,numero_ingresado):
+        pasar_siguiente()
+    
+    nom=nombrepdf()
+    time.sleep(6)
     boton_ver_form= driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div:nth-child(2) > div.submission-actions > a:nth-child(3)")
     boton_ver_form.click()
-
+    time.sleep(6)
     #VENTANA 1 -------------------------------------------
     ventana_form = driver.window_handles
 
     # Cambia el enfoque a la nueva ventana abierta
     # Puedes ajustar el índice de la lista `windows` si hay múltiples ventanas abiertas
     driver.switch_to.window(ventana_form[1])
-
+    #print(len(ventana_form))
     # Espera unos segundos para asegurarse de que la nueva ventana se cargue completamente
     time.sleep(10)
 
     # Encuentra y hace clic en el botón de imprimir en la nueva ventana
     boton_imprimir = driver.find_element(By.CSS_SELECTOR, "body > div.main > article > header > button.form-header__button--print.btn-bg-icon-only")
     boton_imprimir.click()
+   
+    time.sleep(10)
+
 else: 
      alerta("ERROR \n El bot no puede continuar con el proceso de automatizacion ya que no ha seleccionado correctamente ninguno de los formularios")
 
 
 
-# #ver si Peña logra hacerlo controlando el mouse (DARLE AL BOTON IMPRIMIR EN LA VENTANA EMERGENTE DEL PDF)  
-
-# # # Hacer clic en el botón "Imprimir" en la ventana emergente
-# boton_imprimir_guardar= driver.find_element(By.CSS_SELECTOR, '#sidebar > div.c01147 > div > button.c01123.c01154.c01124')
-# boton_imprimir_guardar.click()
-
-
 # Espera unos segundos para que los resultados se carguen las dos lineas de codigo son formas 
 #driver.implicitly_wait(1200)
-time.sleep(30)
+time.sleep(10)
 
 # Cierra el navegador (no se porque igual se cierra solo)
 driver.quit()
