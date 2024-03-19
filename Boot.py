a0e04cce7390305439a45b0c0bd56add8a442768
Alex-Altuve <altuvealex6@gmail.com>
2024-03-19 19:52:26 -0400

    Se arreglo el error que habia en las alertas, se testearon las validaciones (funcionan), se agrego una ventana para que el usuario ingrese el dato necesario para seguir el procedimiento de descarga, falta agregar la parte de codigo que es para completar el proceso de descarga

--- Boot.py
+++ Boot.py
@@ -1,19 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import keyboard
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoAlertPresentException
 import pyautogui  as pa
-import random
 import time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from datetime import datetime, timedelta
+import re
 
-def alerta(mensaje_notificacion):
+
+def alerta(mensaje_notificacion): ## PROBAR
     # Ejecutar JavaScript para mostrar una notificación
     script = f'alert("{mensaje_notificacion}");'
     driver.execute_script(script)
-    time.sleep(10)
+  # Esperar hasta que aparezca la alerta
+    wait = WebDriverWait(driver, 10)
+    while True:
+        try:
+            alert = wait.until(EC.alert_is_present())
+
+            # Verificar si el usuario ha interactuado con la notificación
+            if alert.text == mensaje_notificacion:
+                # Esperar a que el usuario interactúe manualmente
+                while True:
+                    time.sleep(5)  # Pausa de 1 segundo
+                    try:
+                        alert.accept()
+                        break
+                    except NoAlertPresentException:
+                        break
+
+                break
+
+        except NoAlertPresentException:
+            break
 
 def nombrepdf():
     copiando_nombre=driver.find_element(By.CSS_SELECTOR, '#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div.submission-data-table > div.submission-data-table__row.submission-data-table__row--group.submission-data-table__row--type-group_root > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(2) > div.submission-data-table__row.submission-data-table__row--group-children > div:nth-child(16) > div.submission-data-table__column.submission-data-table__column--data > div')
@@ -68,34 +89,41 @@ def pasar_siguiente():
 
 #------VENTANA 0
 # Ruta al controlador de Microsoft Edge (descárgalo previamente desde https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
-# No funciono asi que lo deje asi pero hay que verificar como hacer esto 
+
 print(pa.size())#en la mia es 1920x1080
 print(pa.position())#(x=143, y=961) esto para darle al boton imprimir, (x=170, y=435)Para escribir el nombre, (x=793, y=504)para guardarlo con el nombre 
-# Inicializa las opciones del navegador Edge
+
 # Inicializa las opciones del navegador Chrome
 options = Options()
 options.add_argument("--disable-features=EnableEphemeralFlashPermission")
 
-
 # Crea el controlador del navegador Chrome con las opciones configuradas
 # Inicializa el navegador Microsoft chrome
 driver = webdriver.Chrome(options=options)
-#Esto es para inicializarlo sin configuraciones 
-#driver = webdriver.Edge()
+
 
 # Abre la página de Google
 driver.get('https://eu.kobotoolbox.org/accounts/login/')
 #Se maximiza la pantalla
 driver.maximize_window()
-
 time.sleep(6)
-usuario=driver.find_element(By.NAME, "login")
-usuario.send_keys('pasante_monitoreo')
-contrasena=driver.find_element(By.NAME,'password')
-contrasena.send_keys('1/fyV2g(H1h')
-contrasena.submit()
+
+#PARA PROBAR 
+# usuario=driver.find_element(By.NAME, "login")
+# usuario.send_keys('pasante_monitoreo')
+# contrasena=driver.find_element(By.NAME,'password')
+# contrasena.send_keys('1/fyV2g(H1h')
+# contrasena.submit()
+
+alerta("Debe ingresar su usuario y contraseña para que el bot pueda continuar")
+# Pausa para que el usuario ingrese su usuario y contraseña
+while True:
+    if driver.current_url != 'https://eu.kobotoolbox.org/accounts/login/':
+        break
+    time.sleep(0.1)
+
 # Esperar a que la página se cargue completamente
-driver.implicitly_wait(10)
+driver.implicitly_wait(15)
 
 #Variable necesarias para validacion
 enlaces_url = []
@@ -108,6 +136,8 @@ if( len(enlaces)!=0):
         enlaces_url.append(url_enlace)
 else: 
     alerta("Ha ocurrido un error al continuar con el proceso con el bot, por favor, apague el bot e intente de nuevo")
+    time.sleep(8)
+    driver.quit()
 
 alerta("Debe seleccionar un formulario para que el bot pueda continuar con el proceso de automatización")
 #Pausa para que el usuario seleccione el formulario
@@ -120,6 +150,7 @@ for url_enlace in enlaces_url:
     if driver.current_url == url_enlace :
         validacion=True
         break
+
 #Valida si verdaderamente selecciono un formulario (Esta automatizacion aplica para todos los formularios que seleccione)
 if validacion == True:
     time.sleep(6) 
@@ -129,41 +160,47 @@ if validacion == True:
     time.sleep(6) 
     ojitos = driver.find_elements(By.CSS_SELECTOR,'button[data-tip="Abrir"]')
   
-    numero_ingresado = int(pa.prompt(text='Por favor, ingrese un número desde donde iniciar', default='0'))
-    print("Número ingresado:", numero_ingresado)
-
-#    # #numero_pdf=int(input("Por favor, ingrese la cantidad de archivos a descargar: "))
-    #seleccionamos el primer ojito para llegar al que queremos esto por ahora
-    ojitos[0].click() 
-    time.sleep(6)
-    #le damos tantas veces a siguiente para llegar al archivo que queramos 
-    for i in  range(1,numero_ingresado):
-        pasar_siguiente()
-    
-    nom=nombrepdf()
-    time.sleep(6)
-    boton_ver_form= driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div:nth-child(2) > div.submission-actions > a:nth-child(3)")
-    boton_ver_form.click()
-    time.sleep(6)
-    #VENTANA 1 -------------------------------------------
-    ventana_form = driver.window_handles
-
-    # Cambia el enfoque a la nueva ventana abierta
-    # Puedes ajustar el índice de la lista `windows` si hay múltiples ventanas abiertas
-    driver.switch_to.window(ventana_form[1])
-    #print(len(ventana_form))
-    # Espera unos segundos para asegurarse de que la nueva ventana se cargue completamente
-    time.sleep(10)
-
-    # Encuentra y hace clic en el botón de imprimir en la nueva ventana
-    boton_imprimir = driver.find_element(By.CSS_SELECTOR, "body > div.main > article > header > button.form-header__button--print.btn-bg-icon-only")
-    boton_imprimir.click()
    
-    time.sleep(10)
+    numero_ingresado = pa.prompt(text='Por favor, ingrese un número desde donde iniciar')
+
+    if(re.match("^[0-9]+$", numero_ingresado)):
+        numero= int(numero_ingresado)
+        print("Número convertido:", numero)
+        ojitos[0].click() 
+        time.sleep(6)
+        #le damos tantas veces a siguiente para llegar al archivo que queramos 
+        for i in  range(1,numero):
+            pasar_siguiente()
+        
+        nom=nombrepdf()
+        time.sleep(6)
+        boton_ver_form= driver.find_element(By.CSS_SELECTOR,"#kpiapp > div.mdl-layout.mdl-layout--fixed-header.page-wrapper.page-wrapper--is-modal-visible.page-wrapper--is-modal-submission > div.modal__backdrop > div > div > div > div > div:nth-child(2) > div.submission-actions > a:nth-child(3)")
+        boton_ver_form.click()
+        time.sleep(6)
+        #VENTANA 1 -------------------------------------------
+        ventana_form = driver.window_handles
+
+        # Cambia el enfoque a la nueva ventana abierta
+        # Puedes ajustar el índice de la lista `windows` si hay múltiples ventanas abiertas
+        driver.switch_to.window(ventana_form[1])
+        #print(len(ventana_form))
+        # Espera unos segundos para asegurarse de que la nueva ventana se cargue completamente
+        time.sleep(10)
+
+        # Encuentra y hace clic en el botón de imprimir en la nueva ventana
+        boton_imprimir = driver.find_element(By.CSS_SELECTOR, "body > div.main > article > header > button.form-header__button--print.btn-bg-icon-only")
+        boton_imprimir.click()
+    
+        time.sleep(10)
+    else:
+        alerta("ERROR Solo se aceptan numeros en el campo solicitado anteriormente")
+        time.sleep(8)
+        driver.quit()     
 
 else: 
-     alerta("ERROR \n El bot no puede continuar con el proceso de automatizacion ya que no ha seleccionado correctamente ninguno de los formularios")
-
+    alerta("ERROR El bot no puede continuar con el proceso de automatizacion ya que no ha seleccionado correctamente ninguno de los formularios")
+    time.sleep(8)
+    driver.quit()
 
 
 # Espera unos segundos para que los resultados se carguen las dos lineas de codigo son formas 
